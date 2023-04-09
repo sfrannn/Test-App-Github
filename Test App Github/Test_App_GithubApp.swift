fb9dd3f4cef435f53ffa40cc0ae81760cc18d553
syd <sfranta@wisc.edu>
2023-04-09 13:39:18 -0500

    Initial Commit

---
+++ Test App Github/Test_App_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_App_GithubApp.swift
+//  Test App Github
+//
+//  Created by SYDNEY ELIZABETH FRANTA on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_App_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test App GithubUITests/Test_App_GithubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_App_GithubUITestsLaunchTests.swift
+//  Test App GithubUITests
+//
+//  Created by SYDNEY ELIZABETH FRANTA on 4/9/23.
+//
+
+import XCTest
+
+final class Test_App_GithubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
